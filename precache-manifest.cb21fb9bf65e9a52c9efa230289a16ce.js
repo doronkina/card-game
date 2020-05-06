8faffd4705ee42ca910ec7779ba853c01a906a09
doronkina <liudmyla.doronkina0@gmail.com>
2020-05-06 19:02:23 +0300

    Updates

---
+++ precache-manifest.cb21fb9bf65e9a52c9efa230289a16ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ca41fb04f0ef4f6c1e8527822fdb34",
+    "url": "/card-game/index.html"
+  },
+  {
+    "revision": "cae51e175b26cbca11b0",
+    "url": "/card-game/static/css/main.3513fe97.chunk.css"
+  },
+  {
+    "revision": "77b5c18b795c284ab41e",
+    "url": "/card-game/static/js/2.0d202a90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-game/static/js/2.0d202a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae51e175b26cbca11b0",
+    "url": "/card-game/static/js/main.3e0d0423.chunk.js"
+  },
+  {
+    "revision": "1f9030f83d9496b9407a",
+    "url": "/card-game/static/js/runtime-main.bb0c46b3.js"
+  }
+]);
